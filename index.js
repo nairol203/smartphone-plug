761042165bea1a65fa16ba7663c562a718cb9c15
nairol <f.bock.2003@gmail.com>
2022-06-26 23:42:17 +0200

    Cleaner Code v1

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const axios = require('axios');
 
+const POWER_THRESHOLD = 5;
 let cachedPower;
 
 setInterval(async () => {
-	const { data } = await axios('http://192.168.2.141/cm?cmnd=Status%208');
-	const power = data.StatusSNS.ENERGY.Power;
+	const req = await axios('http://192.168.2.141/cm?cmnd=Status%208').catch(() => null);
+	const voltage = req?.data?.StatusSNS?.ENERGY?.Voltage;
+	const power = req?.data?.StatusSNS?.ENERGY?.Power;
 
-	if (data.StatusSNS.ENERGY.Voltage === 0) return;
+	if (!voltage) return console.log('Power plug not available or turned off');
 
-	if (power <= 5 && cachedPower <= 5) {
+	if (power <= POWER_THRESHOLD && cachedPower <= POWER_THRESHOLD) {
 		await axios('http://192.168.2.141/cm?cmnd=Power%20OFF');
 		console.log('The power plug has been switched off.');
 		cachedPower = undefined;
 		return;
 	}
+
+	console.log(power, cachedPower);
 
 	cachedPower = power;
 }, 30000);
