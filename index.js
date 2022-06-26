2c14521fd58c07567af8cad475e727ea1bdea4a1
nairol <f.bock.2003@gmail.com>
2022-06-26 23:35:45 +0200

    Revert "Cleaner Code"
    This reverts commit 8a0f899dad5124eb980ff1a5bf8177e5a538ec3e.

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 const axios = require('axios');
-const { setTimeout } = require('timers/promises');
 
-const POWER_THRESHOLD = 5;
-
-const getPower = async () => {
-	try {
-		const { data } = await axios('http://192.168.2.141/cm?cmnd=Status%208');
-		const voltage = data.StatusSNS.ENERGY.Voltage;
-		const power = data.StatusSNS.ENERGY.Power;
-		console.log('Request successful', `Voltage: ${voltage}`, `Power: ${power}`);
-
-		if (voltage === 0) return undefined;
-		return power;
-	} catch (error) {
-		console.log('Request failed', error);
-	}
-};
+let cachedPower;
 
 setInterval(async () => {
-	const power1 = await getPower();
-	await setTimeout(30000);
-	const power2 = await getPower();
+	const { data } = await axios('http://192.168.2.141/cm?cmnd=Status%208');
+	const power = data.StatusSNS.ENERGY.Power;
 
-	if (isNaN(power1) || isNaN(power2)) return console.log(`Power undefined`, power1, power2);
-	if (power1 > POWER_THRESHOLD || power2 > POWER_THRESHOLD) return console.log('Power above threshold', power1, power2);
+	if (data.StatusSNS.ENERGY.Voltage === 0) return;
 
-	await axios('http://192.168.2.141/cm?cmnd=Power%20OFF');
-	console.log('The power plug has been switched off.', power1, power2);
+	if (power <= 5 && cachedPower <= 5) {
+		await axios('http://192.168.2.141/cm?cmnd=Power%20OFF');
+		console.log('The power plug has been switched off.');
+		cachedPower = undefined;
+		return;
+	}
+
+	cachedPower = power;
 }, 30000);
